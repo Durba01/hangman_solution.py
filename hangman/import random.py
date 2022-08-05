ea90fcaa65c0987beab880c554f79badc746d933
Muhammad Jibril <muhammaddurba@gmail.com>
2022-08-05 18:37:47 +0100

    hangman_solution.py

---
+++ hangman/import random.py
@@ -0,0 +1,10 @@
+import random
+word_list = ["grape", "pear", "apple", "banana", "berries"]
+
+class Hangman():
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(self.word_list)
+        self.word_guessed = list('-'*len(self.word))
+        self.num_letters = len(set(self.word))
